9a760d707c1764549fa65859a30a9b38ff9de42f
YWHo <YWHo@users.noreply.github.com>
2017-12-06 23:19:44 +1300

    add function isEven()

--- eloquent.js
+++ eloquent.js
@@ -10,6 +10,34 @@ function min(num1, num2) {
     }
 }
 
-console.log(min(0, 10));    // should return 0
-console.log(min(0, -10));   // should return -10
+console.log("\n---- test function min() -----");
+console.log(min(0, 10)); // expect: 0
+console.log(min(0, -10)); // expect: -10
+
+
+/*
+Here’s another way to define whether a positive whole number is even or odd:
+- Zero is even.
+- One is odd.
+-  For any other number N, its evenness is the same as N - 2.
+
+Define a recursive function isEven corresponding to this description. The function should accept a number parameter and return a Boolean.
+*/
+
+function isEven(num) {
+    if (num == 0) {
+        return true;
+    } else if (num == 1) {
+        return false;
+    } else if (num < 0) {
+        return isEven(-num);  // flip negative number to positive
+    } else {
+        return isEven(num - 2);
+    }
+}
+
+console.log("\n---- test function isEven() -----");
+console.log(isEven(50)); // expect: true
+console.log(isEven(75)); // expect: false
+console.log(isEven(-1)); // expect: false
 
